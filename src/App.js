f905dbc23e9f45cfa1f9c1c9415f3a3ca3cfd26e
mk-win <sfanrod@mail.ru>
2024-05-24 13:27:30 +0300

    01.первый push

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Первый PUSH</h1>
+        <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium doloremque ducimus, eligendi explicabo fugit hic modi numquam odit, quia repellat ut veritatis.</h4>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit-2 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
