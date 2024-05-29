ce2618638dda51359a3dff437c283fad4bf1a1c6
mk-win <sfanrod@mail.ru>
2024-05-30 00:19:41 +0300

    07. dev1 zzzzzzzzzzz

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Первый PUSH</h1>
+        <h1>Первый Branch DEV1</h1>
         <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium doloremque ducimus, eligendi explicabo fugit hic modi numquam odit, quia repellat ut veritatis.</h4>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
