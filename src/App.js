7a97cd9b5d02b3089ca63c3828d05822485290e4
mk-win <sfanrod@mail.ru>
2024-05-31 12:43:58 +0300

    07. dev1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <div>
           <div>Dev1-11</div>
           <div>Dev1-12</div>
-          <div>Dev1-131</div>
+          <div>Dev1-13</div>
         </div>
         <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium doloremque ducimus, eligendi explicabo fugit hic modi numquam odit, quia repellat ut veritatis.</h4>
         <img src={logo} className="App-logo" alt="logo" />
